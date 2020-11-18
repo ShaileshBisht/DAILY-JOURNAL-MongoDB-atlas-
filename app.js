85f4aaf450736686c8a80b2371dcf01241d66b1e
shaileh Bisht <shaileshbisht765@gmail.com>
2020-11-18 19:54:01 +0530

    make correction on about page discription

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const _ = require("lodash");
 const mongoose = require("mongoose");
 
 const homeStartingContent = "A diary is a record (originally in handwritten format) with discrete entries arranged by date reporting on what has happened over the course of a day or other period. A personal diary may include a person's experiences, thoughts, and/or feelings, excluding comments on current events outside the writer's direct experience. Someone who keeps a diary is known as a diarist";
-const aboutContent = "This app was created by SHAILESH BISHT from learning and practice purpose .";
+const aboutContent = "This app was created by SHAILESH BISHT for learning and practice purpose .";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
